5400c512dc1ae38eb962a87707646e89d29e8d82
sweta bharti <swetasily@gmail.com>
2021-11-28 14:09:11 +0530

    can_drivesummision

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,24 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    function CanDrive(hasDrivingLiscence, isTired, isSober) {
+        if(hasDrivingLiscence==true && isTired==false &&isSober==true){
+            return "You can drive";
+        }
+        else if(hasDrivingLiscence==false || isTired==true && isSober==true){
+            return "You cannot drive";
+        }
+        else if(hasDrivingLiscence==true || isTired==true && isSober==false){
+            return "You shouldn't drive";
+        }
+        else if(hasDrivingLiscence==true && isTired==false && isSober==false){
+            return "You shouldn't drive";
+        }
+    }
+    
+    module.exports = CanDrive;
+
+
+
 }
 
 module.exports = CanDrive;
